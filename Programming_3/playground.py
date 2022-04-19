107d725236bcba49ba250e2e2fb76aba4c585496
JustinXre2020 <justinxiaoxulong@gmail.com>
2022-04-19 08:48:27 -0500

    new changes

--- Programming_3/playground.py
+++ Programming_3/playground.py
@@ -1,15 +1,18 @@
 import os
 from datetime import datetime
+import time
 
 userinfo = 'chat-history.txt'
 userinfo_path = os.path.join(os.getcwd(), userinfo)         # generate userinfo path
-mode = 'r+' if os.path.exists(userinfo_path) else 'w+'      # set mode (append & read or write & read) 
+mode = 'r+' if os.path.exists(userinfo_path) else 'a'      # set mode (append & read or write & read) 
                                                             # based on the existance of userinfo
+msg = "Howdy world!"
+
 with open(userinfo_path, mode) as f:
     # write the file
-    # for i in range(10):
-        # f.write(f"At {datetime.now()}, BM, Run sends John: Howdy world!\n")
-    f.write("Justin Xiao \n")
+    for i in range(10):
+        f.write(f"At {datetime.now()}, BM, Run sends John: {msg}\n")
+        # f.write("Justin Xiao \n")
        
 
 with open(userinfo_path, mode) as f:
@@ -39,14 +42,14 @@ with open(userinfo_path, mode) as f:
 # except (UnicodeDecodeError, AttributeError):
 #     print(l)
 
-from pg3lib import *
+# from pg3lib import *
 
-# key = getPubKey()
+# # key = getPubKey()
 
-# print(key.decode())
+# # print(key.decode())
 
-# key_server = b'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUF6TXZuLzRDbjNpZis4NzBPUHpQdAo4NVFGRkRvajdpelc3OG1OUElLWWE2WFdFZ2pTK1JNSjVMcTZIYUFielFtclJIeXQ2a2JMQWJPVmY1WWhMRGo1CmNaYzh2RXhuZ1lJTnpIcmlOMEpSa1hJaEp0Nk03ZDBqeXRlR3ZPeHE3eGFEcU5nbC90QlZ0VWNLZWM3Z3BGVlIKWk56djZDcWh1NVc4R2p0OFRNcTF4Mm85dkxIV29UR1R6bmlBSTZMd3lHd0JHTmdLbHRjaCtXa0RvMjZoVk4wdQpHcFRrUkVNQjMrczBJS1ZoazYyQVViR1ZNNU53cFVWTVJPcFNRUHlhRkpKUXdvajJPcFdIemxyTzE5VE1BUnhRCitLTkVTWExQaEZoTURvTk5BQ2QyMFZPb0ZRTUpJcjFyVHU4RVpRZGFxT1ducThQOWVmNHgzVnJGM0ZGMUFKSUQKWHdJREFRQUIKLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg=='
+# # key_server = b'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUF6TXZuLzRDbjNpZis4NzBPUHpQdAo4NVFGRkRvajdpelc3OG1OUElLWWE2WFdFZ2pTK1JNSjVMcTZIYUFielFtclJIeXQ2a2JMQWJPVmY1WWhMRGo1CmNaYzh2RXhuZ1lJTnpIcmlOMEpSa1hJaEp0Nk03ZDBqeXRlR3ZPeHE3eGFEcU5nbC90QlZ0VWNLZWM3Z3BGVlIKWk56djZDcWh1NVc4R2p0OFRNcTF4Mm85dkxIV29UR1R6bmlBSTZMd3lHd0JHTmdLbHRjaCtXa0RvMjZoVk4wdQpHcFRrUkVNQjMrczBJS1ZoazYyQVViR1ZNNU53cFVWTVJPcFNRUHlhRkpKUXdvajJPcFdIemxyTzE5VE1BUnhRCitLTkVTWExQaEZoTURvTk5BQ2QyMFZPb0ZRTUpJcjFyVHU4RVpRZGFxT1ducThQOWVmNHgzVnJGM0ZGMUFKSUQKWHdJREFRQUIKLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg=='
 
-key_client = b'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUF6TXZuLzRDbjNpZis4NzBPUHpQdAo4NVFGRkRvajdpelc3OG1OUElLWWE2WFdFZ2pTK1JNSjVMcTZIYUFielFtclJIeXQ2a2JMQWJPVmY1WWhMRGo1CmNaYzh2RXhuZ1lJTnpIcmlOMEpSa1hJaEp0Nk03ZDBqeXRlR3ZPeHE3eGFEcU5nbC90QlZ0VWNLZWM3Z3BGVlIKWk56djZDcWh1NVc4R2p0OFRNcTF4Mm85dkxIV29UR1R6bmlBSTZMd3lHd0JHTmdLbHRjaCtXa0RvMjZoVk4wdQpHcFRrUkVNQjMrczBJS1ZoazYyQVViR1ZNNU53cFVWTVJPcFNRUHlhRkpKUXdvajJPcFdIemxyTzE5VE1BUnhRCitLTkVTWExQaEZoTURvTk5BQ2QyMFZPb0ZRTUpJcjFyVHU4RVpRZGFxT1ducThQOWVmNHgzVnJGM0ZGMUFKSUQKWHdJREFRQUIKLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg=='
+# key_client = b'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUF6TXZuLzRDbjNpZis4NzBPUHpQdAo4NVFGRkRvajdpelc3OG1OUElLWWE2WFdFZ2pTK1JNSjVMcTZIYUFielFtclJIeXQ2a2JMQWJPVmY1WWhMRGo1CmNaYzh2RXhuZ1lJTnpIcmlOMEpSa1hJaEp0Nk03ZDBqeXRlR3ZPeHE3eGFEcU5nbC90QlZ0VWNLZWM3Z3BGVlIKWk56djZDcWh1NVc4R2p0OFRNcTF4Mm85dkxIV29UR1R6bmlBSTZMd3lHd0JHTmdLbHRjaCtXa0RvMjZoVk4wdQpHcFRrUkVNQjMrczBJS1ZoazYyQVViR1ZNNU53cFVWTVJPcFNRUHlhRkpKUXdvajJPcFdIemxyTzE5VE1BUnhRCitLTkVTWExQaEZoTURvTk5BQ2QyMFZPb0ZRTUpJcjFyVHU4RVpRZGFxT1ducThQOWVmNHgzVnJGM0ZGMUFKSUQKWHdJREFRQUIKLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg=='
 
-encrypt("Howdy world!".encode(), key_client)
+# encrypt("Howdy world!".encode(), key_client)
